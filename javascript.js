b415c9040190e058d26916dde22c4a5e48618d45
Shannareed <shaylashaniya@yahoo.com>
2024-09-15 15:11:43 +0000

    🏔🍧 Checkpoint ./javascript.html:69253210/131 ./javascript.js:69253210/471

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
-function output(message) { message="hello"
+function output(message) { message="Hello Everyone! If you are seeing this message I have properly linked my utility function from a JavaScript file using the script tag and the src attribute."
   window.outputTag.innerHTML = window.outputTag.innerHTML + message;
 }
