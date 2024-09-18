ec8b431580e2e6cfe392e9c5d0e1fe34d9aff8e9
Shannareed <shaylashaniya@yahoo.com>
2024-09-18 18:01:50 +0000

    🌽🎗 Checkpoint ./contact.html:69253210/2083 ./contact.js:69253210/220

---
+++ contact.js
@@ -0,0 +1,2 @@
+function output(message) { message="Hello Everyone! If you are seeing this message I have properly linked my utility function from a JavaScript file using the script tag and the src attribute."
+                          
