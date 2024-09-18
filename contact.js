1380d2c4052d7b5ebf9c148045aa0294da5c9e3a
Shannareed <shaylashaniya@yahoo.com>
2024-09-18 18:25:52 +0000

    🍼🐟 Checkpoint ./contact.js:69253210/527 ./contact.html:69253210/288

--- contact.js
+++ contact.js
@@ -3,3 +3,5 @@ function output(message) {
     "Hello Everyone! If you are seeing this message I have properly linked my utility function from a JavaScript file using the script tag and the src attribute.";
   window.outputTag.innerHTML = window.outputTag.innerHTML + message;
 }
+
+function ask(message) {"Are you 18 years are older"}
