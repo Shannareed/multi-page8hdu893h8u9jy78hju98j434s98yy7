0641758f68985e1102db29e0d1d91a31f962ed79
Shannareed <shaylashaniya@yahoo.com>
2024-09-27 13:13:10 +0000

    🛫🕴 Checkpoint ./contact.js:69253210/53 ./contact.html:69253210/1183 ./index.html:69253210/258 ./gallery.html:69253210/54 ./about.html:69253210/1

--- contact.js
+++ contact.js
@@ -3,5 +3,3 @@ function output(message) {
     "To join the Shhh Shoetique family as a brand ambassador, you must have at least 5,000 followers on any social media platform. Ambassadors are required to post a picture in our attire at least once a week and achieve a minimum of 400 likes on 80% of your posts. We’re excited to see you showcase our styles with your unique flair! ";
   window.outputTag.innerHTML = window.outputTag.innerHTML + message;
 }
-
-function ask(message) {"Are you 18 years are older"}
